961b5341f1a20483ed68e527ede165a40ae72d47
Carlos <awocarlos55@gmail.com>
2023-12-21 16:30:58 +0100

    Signup and Signin views

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+class RegistrationsController < ApplicationController
+  layout 'auth'
+  skip_before_action :authenticate_user, only: [:new, :create]
+
+  def new 
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(registration_params)
+
+    if @user.save
+      login(@user)
+      redirect_to root_path, notice: "User created successfully"
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+  def registration_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+end
